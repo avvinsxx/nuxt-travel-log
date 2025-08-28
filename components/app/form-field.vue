e7b471a744128e377afcce06655355f3e2a075d8
Andrey Vins <avvins@yandex.ru>
2025-08-28 12:56:56 +0700

    Add location creation

---
+++ components/app/form-field.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  name: string;
+  error?: string;
+  type?: "text" | "textarea";
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ props.label }}
+    </legend>
+    <Field
+      :as="type || 'input'"
+      :name="props.name"
+      :type="props.type || 'text'"
+      :disabled="props.disabled"
+      class="w-full"
+      :class="{
+        'input-error': props.error,
+        'input': !props.type || props.type === 'text',
+        'textarea': props.type === 'textarea',
+      }"
+    />
+    <p v-if="props.error" class="fieldset-label text-error">
+      {{ props.error }}
+    </p>
+  </fieldset>
+</template>
