ff7056c3d6585abccc03dd927c7072900a075f3c
Andrey Vins <avvins@yandex.ru>
2025-08-29 20:13:18 +0700

    Add Map

---
+++ components/app/map.client.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { CENTER_BARNAUL } from "~/lib/constants";
+
+const colorMode = useColorMode();
+const style = computed(() =>
+  colorMode.value === "dark"
+    ? "/styles/dark.json"
+    : "https://tiles.openfreemap.org/styles/liberty",
+);
+const zoom = 3;
+</script>
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="CENTER_BARNAUL"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
