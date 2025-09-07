3d0c847cb9520d57b8ad3ac6c6215a0d431901dd
avvins <avvins@yandex.ru>
2025-09-07 23:00:58 +0700

    Add location deletion (#73)

---
+++ server/api/locations/[slug].delete.ts
@@ -0,0 +1,20 @@
+import { removeLocationBySlug } from "~/lib/db/queries/location";
+import { defineAuthenticatedEventHandler } from "~/utils/define-authenticated-event-handler";
+
+export default defineAuthenticatedEventHandler(async (event) => {
+  return sendError(event, createError({
+    statusCode: 404,
+    statusMessage: "OhNO.",
+  }));
+
+  const slug = getRouterParam(event, "slug") as string;
+  const deleted = await removeLocationBySlug(slug, event.context.user.id);
+  if (!deleted) {
+    return sendError(event, createError({
+      statusCode: 404,
+      statusMessage: "Location not found.",
+    }));
+  }
+
+  setResponseStatus(event, 204);
+});
