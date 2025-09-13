b95acbc98a1b7ce679e5541b3662e954db5d7cd4
Andrey Vins <avvins@yandex.ru>
2025-09-13 13:44:15 +0700

    Add travel log creation

--- server/api/locations/[slug].delete.ts
+++ server/api/locations/[slug].delete.ts
@@ -2,11 +2,6 @@ import { removeLocationBySlug } from "~/lib/db/queries/location";
 import { defineAuthenticatedEventHandler } from "~/utils/define-authenticated-event-handler";
 
 export default defineAuthenticatedEventHandler(async (event) => {
-  return sendError(event, createError({
-    statusCode: 404,
-    statusMessage: "OhNO.",
-  }));
-
   const slug = getRouterParam(event, "slug") as string;
   const deleted = await removeLocationBySlug(slug, event.context.user.id);
   if (!deleted) {
