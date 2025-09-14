c324064340466ce65b96f02bb72c096f47540b99
avvins <avvins@yandex.ru>
2025-09-14 13:59:47 +0700

    Add log editing (#78)

--- pages/dashboard/location/[slug]/[id].vue
+++ pages/dashboard/location/[slug]/[id].vue
@@ -10,8 +10,10 @@ const {
 const loading = computed(() => status.value === "pending");
 const errorMessage = computed(() => error.value?.statusMessage);
 
-onMounted(() => {
-  locationsStore.refreshCurrentLocationLog();
+onBeforeRouteUpdate((to) => {
+  if (to.name === "dashboard-location-slug-id") {
+    locationsStore.refreshCurrentLocationLog();
+  }
 });
 </script>
 
@@ -42,6 +44,9 @@ onMounted(() => {
       <p class="text-sm">
         {{ locationLog.description }}
       </p>
+    </div>
+    <div v-else>
+      <NuxtPage />
     </div>
   </div>
 </template>
