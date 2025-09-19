fbb4c54b0dd1989b97aee972381dea1f7601d5fc
Andrey Vins <avvins@yandex.ru>
2025-09-19 23:12:25 +0700

    Add log images upload

--- lib/db/schema/location-log.ts
+++ lib/db/schema/location-log.ts
@@ -5,8 +5,11 @@ import z from "zod";
 
 import { DescriptionSchema, LatSchema, LongSchema, NameSchema } from "~/lib/zod-schemas";
 
+import type { SelectLocationLogImage } from "./location-log-image";
+
 import { user } from "./auth";
 import { location } from "./location";
+import { locationLogImage } from "./location-log-image";
 
 export const locationLog = sqliteTable("locationLog", {
   id: int().primaryKey({ autoIncrement: true }),
@@ -22,10 +25,13 @@ export const locationLog = sqliteTable("locationLog", {
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
 
-export const locationLogRelations = relations(locationLog, ({ one }) => ({ location: one(location, {
-  fields: [locationLog.locationId],
-  references: [location.id],
-}) }));
+export const locationLogRelations = relations(locationLog, ({ one, many }) => ({
+  location: one(location, {
+    fields: [locationLog.locationId],
+    references: [location.id],
+  }),
+  images: many(locationLogImage),
+}));
 
 export const InsertLocationLog = createInsertSchema(locationLog, {
   name: NameSchema,
@@ -55,3 +61,4 @@ export const InsertLocationLog = createInsertSchema(locationLog, {
 
 export type InsertLocationLog = z.infer<typeof InsertLocationLog>;
 export type SelectLocationLog = typeof locationLog.$inferSelect;
+export type SelectLocationLogWithImages = SelectLocationLog & { images: SelectLocationLogImage[] };
